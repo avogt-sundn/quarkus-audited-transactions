96ec9faad804a6f7843b06c492645f4f62210169
Armin Vogt <armin.vogt@sn-invent.de>
2020-06-07 19:19:36 +0200

    native build finishes successfully

--- src/main/java/org/acme/hibernate/envers/historized/impl/CustomRevisionEntity.java
+++ src/main/java/org/acme/hibernate/envers/historized/impl/CustomRevisionEntity.java
@@ -1,11 +1,13 @@
 package org.acme.hibernate.envers.historized.impl;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
 
 import javax.persistence.Entity;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity(name = "CustomRevisionEntity")
 @RevisionEntity(CustomRevisionEntityListener.class)
 @Data
