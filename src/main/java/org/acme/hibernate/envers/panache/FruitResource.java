d59ed1b84d68dd3b37a9395a1f820ec5c5ccc46a
Armin Vogt <armin.vogt@sn-invent.de>
2020-06-09 11:01:21 +0200

    constants

--- src/main/java/org/acme/hibernate/envers/panache/FruitResource.java
+++ src/main/java/org/acme/hibernate/envers/panache/FruitResource.java
@@ -12,20 +12,21 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.UUID;
 
 ;
 
 @Path("fruits")
-@Produces("application/json")
-@Consumes("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 @Transactional
 @RequestScoped
 @NoArgsConstructor
 @Slf4j
 public class FruitResource extends HistorizedResource<Fruit, UUID> {
-    
+
     @Inject
     public FruitResource(EntityManager entityManager) {
         super(Fruit.class, entityManager);
